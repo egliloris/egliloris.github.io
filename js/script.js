a9f587bfff5c357dd83776d0507ef58d54e8ee86
lolobaut <egliloris2@gmail.com>
2023-04-04 21:22:17 +0200

    LanguageRework
    language-switcher, + german as the standard

--- js/script.js
+++ js/script.js
@@ -3,10 +3,96 @@ const textColor = document.querySelector('#text-color');
 const iconColor = document.querySelector('#accent-color');
 const versionOld = document.querySelector('#reset');
 const version2 = document.querySelector('#ver2');
+const versionOld_de = document.querySelector('#reset_de');
+const version2_de = document.querySelector('#ver2_de');
 var colorchanging = false;
 const settingsButton = document.querySelector('#settings-button');
 const settings = document.querySelector('#settings');
 const inner_settings = document.querySelector('#inner_settings');
+
+
+/*********************** */
+/* Language switcher     */
+/*********************** */
+
+/*set every style of an element with class "language_en" display:none*/
+function hideEnglish(){
+    var elements = document.getElementsByClassName("language_en");
+    for(var i = 0; i < elements.length; i++){
+        elements[i].style.display = "none";
+    }
+}
+
+/*set every style of an element with class "language_de" display:none*/
+function hideGerman(){
+    var elements = document.getElementsByClassName("language_de");
+    for(var i = 0; i < elements.length; i++){
+        elements[i].style.display = "none";
+    }
+}
+
+/*set every style of an element with class "language_en" display:block*/
+function showEnglish(){
+    var elements = document.getElementsByClassName("language_en");
+    for(var i = 0; i < elements.length; i++){
+        elements[i].style.display = "block";
+    }
+}
+
+/*set every style of an element with class "language_de" display:block*/
+function showGerman(){
+    var elements = document.getElementsByClassName("language_de");
+    for(var i = 0; i < elements.length; i++){
+        elements[i].style.display = "block";
+    }
+}
+
+function setLanguageDe(){
+    hideEnglish();
+    showGerman();
+    //set html lang attribute to de
+    document.querySelector('html').setAttribute('lang', 'de');
+
+    window.sessionStorage.setItem('language', 'de');
+}
+
+function setLanguageEn(){
+    hideGerman();
+    showEnglish();
+    //set html lang attribute to en
+    document.querySelector('html').setAttribute('lang', 'en');
+
+    window.sessionStorage.setItem('language', 'en');
+}
+
+
+//change language if German is clicked
+document.querySelector('#language_switcher_DE').addEventListener('click', () => {
+    setLanguageDe();
+});
+
+//change language if English is clicked
+document.querySelector('#language_switcher_EN').addEventListener('click', () => {
+    setLanguageEn();
+});
+
+
+//initially set language to english if language is set in SessionStorage
+if(window.sessionStorage.getItem('language') == 'en'){
+    setLanguageEn();
+}
+else{
+    setLanguageDe();
+}
+
+
+
+
+
+/*********************** */
+/* Color switcher(s)     */
+/*********************** */
+
 
 backgroundColor.addEventListener('change', (event) => {
     document.documentElement.style.setProperty('--color_Background', event.target.value);
@@ -32,7 +118,32 @@ versionOld.addEventListener('click', (event) => {
     iconColor.value = "#379683";
 });
 
+
+
 version2.addEventListener('click', (event) => {
+    document.documentElement.style.setProperty('--color_Background', "#e3f1ff");
+    document.documentElement.style.setProperty('--color_Text', "#000");
+    document.documentElement.style.setProperty('--color_Highlighted', "#f2ffff");
+    document.documentElement.style.setProperty('--color_Icons', '#379683');
+    backgroundColor.value = "#e3f1ff";
+    textColor.value = "#000000";
+    iconColor.value = "#379683";            
+});
+
+
+versionOld_de.addEventListener('click', (event) => {
+    document.documentElement.style.setProperty('--color_Background', "#5CDB95");
+    document.documentElement.style.setProperty('--color_Text', "#EDF5E1");
+    document.documentElement.style.setProperty('--color_Highlighted', "#6beaa4");
+    document.documentElement.style.setProperty('--color_Icons', '#379683');
+    backgroundColor.value = "#5CDB95";
+    textColor.value = "#EDF5E1";
+    iconColor.value = "#379683";
+});
+
+
+
+version2_de.addEventListener('click', (event) => {
     document.documentElement.style.setProperty('--color_Background', "#e3f1ff");
     document.documentElement.style.setProperty('--color_Text', "#000");
     document.documentElement.style.setProperty('--color_Highlighted', "#f2ffff");
@@ -94,6 +205,11 @@ settings.addEventListener('click', () => {
     }
 });
 
+/*********************** */
+/* Scrollbar switcher    */
+/*********************** */
+
+
 //custom scrollbars css loader
 document.querySelector('#switch').addEventListener('click', () => {
  
@@ -110,7 +226,6 @@ document.querySelector('#switch').addEventListener('click', () => {
         document.querySelector('link[href="css/scrollbars.css"]').remove();
     }
 });
-
 
 //default: load the css scrollbars.css and check the switch
 const link = document.createElement('link');
